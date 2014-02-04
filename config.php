a05becfd5b3dd6a53485f4323f2f495f4c25951c
rochelle <shielmtongol@yahoo.com>
2014-02-05 06:18:22 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+$con =array(
+	'host' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'exam_db'
+	);
+$db =new mysqli(
+	$con['host'],
+	$con['username'],
+	$con['password'],
+	$con['database']
+	);
+if(mysqli_connect_errno()){
+	echo 'Could not connect to the database.';
+	exit;
+}
+ ?>
