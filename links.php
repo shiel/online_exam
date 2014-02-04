6211d2bdf705eebab2bdb5fbe256efd584e50e9c
rochelle <shielmtongol@yahoo.com>
2014-02-05 06:19:30 +0800

    Create links.php

---
+++ links.php
@@ -0,0 +1,14 @@
+<link type='text/css' rel='stylesheet' href='css/bootstrap.min.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-responsive.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-responsive.min.css' />
+<link type='text/css' rel='stylesheet' href='css/docs.css' />
+<link type='text/css' rel='stylesheet' href='css/prettify.css' />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/fontello.css" />
+
+
+<script type='text/javascript' src='js/bootstrap.js'/></script>
+<script type="text/javascript" src="js/jquery.1.10.2.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
